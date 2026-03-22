307d2ba0072ab18ea1936b9619c4573da7ef60f8
koeijaanso <88194667+koeijaanso@users.noreply.github.com>
2026-03-22 23:38:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "  `БАГ: описание проблемы`\n"
         "  `УЛУЧШЕНИЕ: твоя идея`\n\n"
         "📌 Все отчёты отправляются разработчику автоматически.\n"
-        "Спасибо за помощь в улучшении проекта! 🚀"
+        "Спасибо за помощь в улучшении проекта! 🚀\n"
+        "  `Ссылка для скачивания: https://drive.google.com/file/d/1_rPmdG-Dna21I24xAGwEJ-SE60jhUs5c/view?usp=sharing`"
+
     )
     
     await update.message.reply_text(welcome_text, parse_mode='Markdown')
